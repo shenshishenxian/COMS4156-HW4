8a3b38ca267a6cd8d5edf09e7c03411d5bcc9063
Christina Floristean <cfloristean@nygenome.org>
2017-10-03 19:58:56 -0400

    added db stuff

--- Show places/flaskapp.py
+++ Show places/flaskapp.py
@@ -3,6 +3,7 @@ from sqlalchemy import *
 from flask import Flask, request, render_template, g, redirect, Response
 import traceback
 from mst import Graph, Vertex, Edge
+import re
 
 DATABASEURI = "postgresql://fortytwo:coms4156@coms4156instance.cf4dw5ld7jgf.us-east-2.rds.amazonaws.com:5432/ToyComs4156"
 engine = create_engine(DATABASEURI)
@@ -27,34 +28,53 @@ scenic_by_key = {scenic.key: scenic for scenic in scenics}
 
 @app.before_request
 def before_request():
-  """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request.
-
-  The variable g is globally accessible.
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print "uh oh, problem connecting to database"
-    traceback.print_exc()
-    g.conn = None
+    try:
+        g.conn = engine.connect()
+    except:
+        print "uh oh, problem connecting to database"
+        traceback.print_exc()
+        g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
+    try:
+        g.conn.close()
+    except Exception as e:
+        pass
 
 @app.route("/_findroute")
 def findroute():
-	username = request.args.get('a', 0, type=str)
-	coordinates = request.args.get('b', 0, type=str)
-	"""
-	return jsonify(result=b)	
-	"""
+    username = request.args.get('a', 0, type=str)
+    coordinates = request.args.get('b', 0, type=str)
+    key = request.args.get('c', 0, type=str)
+    print key
+    graph = Graph()
+    order_num = 0
+    coordinates = coordinates.split(';')
+    coordinates = list(filter(None, coordinates))
+    for location in coordinates:
+        #location = location.replace(" ", "")
+        latlong = re.split(r'[(,)\s]+', location)
+        latlong = list(filter(None, latlong))
+        #latlong = location.split('(,)')
+        order_num += 1
+        v = Vertex('location' + str(order_num), latlong[0], latlong[1])
+        graph.vertices['location' + str(order_num)] = v
+     
+    for source in graph.vertices:
+        for target in graph.vertices:
+            if source != target:
+                graph.add_edge(source, target, 1)
+
+    first_stop = 'location' + '1'
+    new_graph = graph.get_min_spanning_tree(first_stop)
+    new_graph.preorder(new_graph.vertices[first_stop])
+
+    save_route(username, new_graph.route)
+    render_template('map.html', sceni)
+    """
+    return jsonify(result=b)    
+    """
 @app.route("/")
 def  index():
     return render_template('index.html',scenics = scenics)
@@ -66,31 +86,39 @@ def show_scenic(scenic_code):
         return render_template('map.html', scenic = scenic)
     else:
         abort(404)
+# @app.route("/calculate_route", methods=['POST'])
+# def calculate_route(user, destinations):
+#     # order = 0
+#     # for location in destinations:
+#     #     location_id = g.conn.execute('INSERT INTO locations (name,latitude,longitude) VALUES (%s, %s, %s)', city, location[0],location[1])
+#     #     order += 1
+#     #     g.conn.execute('INSERT INTO user_route (user_id) VALUES (%s)', user)
 
-@app.route("/calculate_route", methods=['POST'])
-def calculate_route(city, destinations):
-    # order = 0
-    # for location in destinations:
-    #     location_id = g.conn.execute('INSERT INTO locations (name,latitude,longitude) VALUES (%s, %s, %s)', city, location[0],location[1])
-    #     order += 1
-    #     g.conn.execute('INSERT INTO user_route (user_id) VALUES (%s)', user)
+def save_route(user, route_stops):
+    #create username if none
+    #create user_route
+    #create location obj
+    #create location_link
 
-    graph = Graph()
-    order_num = 0
-    for location in destinations:
-        order_num += 1
-        v = Vertex(city + str(order_num), location[0], location[1])
-        graph.vertices[city + str(order_num)] = v
-       
-        for source in graph.vertices:
-            for target in graph.vertices:
-                if source != target:
-                    graph.add_edge(source, target, 1)
+    cur = g.conn.execute('SELECT user_id FROM map_user WHERE username = \'%s\'' % (user))
+    row = cur.fetchone()
+    if not row:
+        cur = g.conn.execute('INSERT INTO map_user (username) VALUES (\'%s\') RETURNING user_id' % (user))
+        user_id = cur.fetchone()[0]
+    else:
+        user_id = row['user_id']
+    
+    cur = g.conn.execute('INSERT INTO user_route (user_id) VALUES (\'%s\') RETURNING route_id' % (user_id))
+    route_id = cur.fetchone()[0]
 
-    new_graph = graph.get_min_spanning_tree(city + '1')
-    new_graph.preorder(city + '1')
-    print new_graph.route
-    # else:
-    #     abort(404)
+    ordernum = 0
+    for route in route_stops:
+        ordernum += 1
+        cur = g.conn.execute('INSERT INTO location (name, latitude, longitude) VALUES (\'%s\', \'%s\', \'%s\') RETURNING location_id' % (route.name, route.latitude, route.longitude))
+        location_id = cur.fetchone()[0]
+        cur = g.conn.execute('INSERT INTO route_location_link (route_id, location_id, order_num) VALUES (\'%s\', \'%s\', \'%s\')' % (route_id, location_id, ordernum))
+    cur.close()
+    return 0
+
 
 app.run(debug = True)

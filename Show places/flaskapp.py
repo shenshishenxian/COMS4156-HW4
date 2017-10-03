390d703955db6f69c299b96c5103d4b8d5093f31
Christina Floristean <cfloristean@nygenome.org>
2017-10-03 08:33:53 -0400

    mst not integrated into flaskapp yet, opened db connection"

--- Show places/flaskapp.py
+++ Show places/flaskapp.py
@@ -1,33 +1,63 @@
 from flask import Flask, render_template, abort
+from sqlalchemy import *
+from flask import Flask, request, render_template, g, redirect, Response
+import traceback
 
+DATABASEURI = "postgresql://fortytwo:coms4156@coms4156instance.cf4dw5ld7jgf.us-east-2.rds.amazonaws.com:5432/ToyComs4156"
+engine = create_engine(DATABASEURI)
 
 app = Flask(__name__)
 class Scenic:
-	def __init__(self, key, name, lat, lng):
-		self.key = key
-		self.name = name
-		self.lat = lat
-		self.lng = lng
+    def __init__(self, key, name, lat, lng):
+        self.key = key
+        self.name = name
+        self.lat = lat
+        self.lng = lng
 
 scenics = {
-	Scenic('NY', 'New York City', 40.730610, -73.935242),
-	Scenic('PA', 'Paris',48.864716, 2.349014),
-	Scenic('BJ','Beijing',39.913818	,116.363625),
-	Scenic('LD','London',51.508530	,-0.076132)
+    Scenic('NY', 'New York City', 40.730610, -73.935242),
+    Scenic('PA', 'Paris',48.864716, 2.349014),
+    Scenic('BJ','Beijing',39.913818 ,116.363625),
+    Scenic('LD','London',51.508530  ,-0.076132)
 }
 
 scenic_by_key = {scenic.key: scenic for scenic in scenics}
 
+
+@app.before_request
+def before_request():
+  """
+  This function is run at the beginning of every web request 
+  (every time you enter an address in the web browser).
+  We use it to setup a database connection that can be used throughout the request.
+
+  The variable g is globally accessible.
+  """
+  try:
+    g.conn = engine.connect()
+  except:
+    print "uh oh, problem connecting to database"
+    traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+
+
 @app.route("/")
 def  index():
-	return render_template('index.html',scenics = scenics)
+    return render_template('index.html',scenics = scenics)
 
 @app.route("/<scenic_code>")
 def show_scenic(scenic_code):
-	scenic = scenic_by_key.get(scenic_code)
-	if scenic:
-		return render_template('map.html', scenic = scenic)
-	else:
-		abort(404)
+    scenic = scenic_by_key.get(scenic_code)
+    if scenic:
+        return render_template('map.html', scenic = scenic)
+    else:
+        abort(404)
 
 app.run(debug = True)
---
+++ Show places/mst.py
@@ -0,0 +1,175 @@
+from geopy.distance import great_circle
+from geopy.distance import vincenty
+from geopy.distance import distance
+from queue import PriorityQueue
+
+class Vertex(object):
+
+    def __init__(self, vertexName, latitude, longitude):
+        self.name = vertexName
+        self.latitude = latitude
+        self.longitude = longitude
+        self.adjacent_edges = []
+        self.cose = 0
+        self.visited = False
+        self.prev_vertex = None
+
+class Edge(object):
+    def __init__(self, source, target, cost):
+        self.source_vertex = source
+        self.target_vertex = target
+        self.cost = cost
+
+class Graph(object):
+
+    # Finds the Euclidean distance between two points, which is the weight of
+    # the edge.
+    def compute_costs(self):
+        for v in self.vertices.values():
+            for e in v.adjacent_edges:
+                source = (e.source_vertex.latitude, e.source_vertex.longitude)
+                target = (e.target_vertex.latitude, e.target_vertex.longitude)
+                e.cost = distance(source, target).miles
+                print v.name
+                print e.source_vertex.name
+                print e.target_vertex.name
+                print e.cost
+                print '\n'
+     
+
+     #        /**
+     # * Use Prim's algorithm to get a minimum spanning tree.
+     # * 
+     # * @param s
+     # *            Starting Vertex.
+     # * @return Graph of minimum spanning tree.
+     # */
+    def get_min_spanning_tree(self, start_vertex):
+        graph = Graph()
+
+        # Add weights and perform Prim's algorithm.
+        self.compute_costs()
+        self.do_prim(start_vertex)
+
+        # Add all the vertices to the new graph.
+        for v in self.vertices.values():
+            if v.name not in graph.vertices:
+                new_vertex = Vertex(v.name, v.latitude, v.longitude)
+                graph.vertices[new_vertex.name] = new_vertex
+
+        # Go through each vertex and add the edge connecting
+        # the previous node to the current one.
+        for v in self.vertices.values():
+            c = 0
+            if v.prev_vertex:
+                for e in v.adjacent_edges:
+                    # print v.name
+                    # print v.prev_vertex.name
+                    # print '\n'
+                    #Find weight of edge then stop searching.
+                    if e.target_vertex == v.prev_vertex:
+                        c = e.cost
+                        break
+                graph.add_edge(v.prev_vertex.name, v.name, c)
+
+        return graph
+
+    # /**
+    #  * Prim's algorithm to find a minimum spanning tree.
+    #  * 
+    #  * @param s
+    #  *            Starting vertex.
+    #  */
+    def do_prim(self, start_vertex):
+        queue = PriorityQueue()
+
+        # Set each vertex to infinite cost and not visited.
+        for v in self.vertices.values():
+            v.cost = float("inf")
+            v.visited = False
+        # Add the starting point to the queue.
+        self.vertices[start_vertex].visited = True
+        self.vertices[start_vertex].cost = 0
+        queue.put((self.vertices[start_vertex].cost, self.vertices[start_vertex]))
+        while not queue.empty():
+
+            # Visit the minimum value.
+            cur_vertex = queue.get()[1]
+            print 'out of queue: ' + cur_vertex.name
+            cur_vertex.visited = True
+
+            #For every edge of every vertex.
+            #print cur_vertex.adjacent_edges
+            for e in cur_vertex.adjacent_edges:
+                e.target_vertex.name
+                # If the edge is adjacent and the vertex has not already
+                # been visited.
+                if not e.target_vertex.visited:
+
+                    # Remove the temporary infinite value of v and
+                    # replace it with its actual cost.
+                    #print e.target_vertex.cost
+                    if e.cost < e.target_vertex.cost:
+                        e.target_vertex.cost = e.cost
+                        e.target_vertex.prev_vertex = cur_vertex
+                    queue.put((e.target_vertex.cost, e.target_vertex))
+                    print 'in queue: ' + e.target_vertex.name
+
+
+    # /**
+    #  * Add a new edge from u to v. Create new nodes if these nodes don't exist
+    #  * yet. This method permits adding multiple edges between the same nodes.
+    #  * 
+    #  * @param u
+    #  *            the source vertex.
+    #  * @param w
+    #  *            the target vertex.
+    #  */
+    def add_edge(self, nameU, nameV, cost):
+        if not self.vertices[nameU]:
+            self.vertices[nameU.name] = nameU
+        if not self.vertices[nameV]:
+            self.vertices[nameV.name] = nameV
+        source_vertex = self.vertices[nameU]
+        target_vertex = self.vertices[nameV]
+        new_edge = Edge(source_vertex, target_vertex, cost)
+        source_vertex.adjacent_edges.append(new_edge)
+
+    def _run(self):
+        v1 = Vertex('New York City', 40.730610, -73.935242)
+        v2 = Vertex('Paris',48.864716, 2.349014)
+        v3 = Vertex('Beijing',39.913818 ,116.363625)
+        v4 = Vertex('London',51.508530  ,-0.076132)
+        self.vertices['New York City'] = v1
+        self.vertices['Paris'] = v2
+        self.vertices['Beijing'] = v3
+        self.vertices['London'] = v4
+  
+        for source in self.vertices:
+            #self.vertices[source].adjacent_edges = []
+            for target in self.vertices:
+                if source != target:
+                    self.add_edge(source,target,1)
+      
+        #self.print_adj_list()
+        new_graph = self.get_min_spanning_tree(v1.name)
+        
+        new_graph.print_adj_list()
+
+    def print_adj_list(self):
+        for u in self.vertices:
+            s = u
+            s += " -> [ "
+            for e in self.vertices[u].adjacent_edges:
+                s += e.target_vertex.name
+                s += "("
+                s += str(e.cost)
+                s += ") "
+            s += "]"
+            print s
+
+    def __init__(self):
+        self.vertices = {}
+    
+if __name__ == '__main__':
+    Graph()._run()

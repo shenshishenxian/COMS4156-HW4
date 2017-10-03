8c1dd69d8002de359934f09f869980b5495f2ef7
Christina Floristean <cfloristean@nygenome.org>
2017-10-03 15:04:41 -0400

    changed a few things on front end

--- Show places/flaskapp.py
+++ Show places/flaskapp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, abort
 from sqlalchemy import *
 from flask import Flask, request, render_template, g, redirect, Response
 import traceback
+from mst import Graph, Vertex, Edge
 
 DATABASEURI = "postgresql://fortytwo:coms4156@coms4156instance.cf4dw5ld7jgf.us-east-2.rds.amazonaws.com:5432/ToyComs4156"
 engine = create_engine(DATABASEURI)
@@ -59,5 +60,31 @@ def show_scenic(scenic_code):
         return render_template('map.html', scenic = scenic)
     else:
         abort(404)
+
+@app.route("/calculate_route", methods=['POST'])
+def calculate_route(city, destinations):
+    # order = 0
+    # for location in destinations:
+    #     location_id = g.conn.execute('INSERT INTO locations (name,latitude,longitude) VALUES (%s, %s, %s)', city, location[0],location[1])
+    #     order += 1
+    #     g.conn.execute('INSERT INTO user_route (user_id) VALUES (%s)', user)
+
+    graph = Graph()
+    order_num = 0
+    for location in destinations:
+        order_num += 1
+        v = Vertex(city + str(order_num), location[0], location[1])
+        graph.vertices[city + str(order_num)] = v
+       
+        for source in graph.vertices:
+            for target in graph.vertices:
+                if source != target:
+                    graph.add_edge(source, target, 1)
+
+    new_graph = graph.get_min_spanning_tree(city + '1')
+    new_graph.preorder(city + '1')
+    print new_graph.route
+    # else:
+    #     abort(404)
 
 app.run(debug = True)
